509d9489c628211bafe347fb16dd15162c8798cb
Jared Kelly <jared.kelly@deftpower.com>
2026-03-17 15:34:22 +0100

    refactor

--- todo.py
+++ todo.py
@@ -1,11 +1,10 @@
 from os import listdir
 from textual.app import App
-from textual.containers import Container
+from textual.containers import Container, Vertical, Horizontal
 from textual.screen import ModalScreen
-from textual.widgets import Footer, Header, Input, Label, ListView, ListItem
+from textual.widgets import Footer, Header, Input, Label, ListView, ListItem, Static
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
-
 
 ### CONFIG ###
 from pathlib import Path
@@ -18,104 +17,132 @@ APP_TITLE = "Jared's Todo List"
 global CURRENT_LIST
 
 class TodoApp(App):
+    CSS = """
+        Horizontal {
+            height: 1fr;
+        }
+
+        Horizontal Label {
+            width: 1fr;
+        }
+
+        Input {
+            width: 100%;
+        }
+    """
+
     TITLE = APP_TITLE
     
     BINDINGS = [
-        ("q", "quit", "quit"),
-        ("a", "add_todo", "add"),
-        ("d", "delete_todo", "delete"),
-        ("e", "edit_todo", "edit"),
-        ("c", "select_category", "select category"),
-        ("shift+up", "move_todo_up", "move up"),
-        ("shift+down", "move_todo_down", "move down")
+        ("q", "quit", "quit")
     ]
 
     theme = "dracula"
 
     def __init__(self, todos: list[str]):
         super().__init__()
-        self.todos = todos        
-        self.todo_list_view = TodoListView()
+        self.todo_list_view = TodoListView(todos)
+        self.done_list_view = DoneListView()
         self.input = Input()
-        self.editing: int = -1 
     
     def compose(self):
         yield Header(show_clock=True, icon='🖊️')
+        with Vertical():
+            with Horizontal():
+                yield self.todo_list_view
+                yield self.done_list_view
+            yield self.input
+
         yield Footer()
-        yield self.todo_list_view
-        yield self.input
 
     def on_mount(self):
-        self.display_todos()
-            
-    def display_todos(self):
-        self.todo_list_view.clear()
-
-        if len(self.todos) == 0:
-            self.todo_list_view.append(TodoListItem("No todos in list! Press 'a' to add one."))
-            return
-
-        for todo in self.todos:
-            list_item = TodoListItem(todo)
-            self.todo_list_view.append(list_item)
-            
-    def save_todos(self):
-        export_todo_list(todos)
+        self.todo_list_view.update_list_display()
 
     def on_input_submitted(self, submitted: Input.Submitted) -> None:
-        self.input.visible = False
-        
-        if self.editing == -1:
-            self.todos.append(submitted.value)
-        else:
-            self.todos[self.editing] = submitted.value
-        
-        self.editing = -1 # set back to "add" mode
-        self.save_todos()
-        self.display_todos()
-
-    def action_add_todo(self):
+        self.todo_list_view.save_or_edit_todo(submitted.value)
         self.input.value = ""
-        self.input.visible = True
-        self.input.focus()
+    
+    def focus_list_view(self, index: int):
+        self.todo_list_view.index = index
+        self.todo_list_view.focus()
 
-    def action_edit_todo(self):
-        index = self.todo_list_view.index
-        label = self.todo_list_view.children[index].children[0]
-        self.editing = index
-        self.input.value = label.content
-        self.input.visible = True
+    def focus_input(self, text=""):
+        self.input.value = text
         self.input.focus()
-        
-    def action_delete_todo(self):
-        index = self.todo_list_view.index
-        self.todos.pop(index)
-        self.save_todos()
-        self.display_todos()
         
     def action_select_category(self):
         self.push_screen(SelectCategoryScreen())
 
+
+class TodoListView(ListView):
+    BINDINGS = {
+        ("a", "add_todo", "add"),
+        ("d", "delete_todo", "delete"),
+        ("e", "edit_todo", "edit"),
+        ("c", "select_category", "select category"),
+        ("shift+up", "move_todo_up", "move up"),
+        ("shift+down", "move_todo_down", "move down")
+    }
+
+    def __init__(self, todos: list[str]):
+        super().__init__(initial_index=1)
+        self.todos = todos
+        # self.app = app
+        self.border_title = "Todo"
+        self.styles.border = ("heavy", "white")
+        self.editing: int = -1 
+
+    def action_edit_todo(self):
+        label = self.children[self.index].children[0]
+        self.editing = self.index
+        self.app.focus_input(label.content)
+        
+    def action_delete_todo(self):
+        self.todos.pop(self.index)
+        export_todo_list(todos)
+        self.update_list_display()
+
+    def action_add_todo(self):
+        self.app.focus_input()
+
+    def save_or_edit_todo(self, todo: str):
+        if self.editing == -1:
+            self.todos.append(todo)
+        else:
+            self.todos[self.editing] = todo
+        
+        self.editing = -1 # set back to "add" mode
+        export_todo_list(todos)
+        self.update_list_display()
+        self.call_after_refresh(self.app.focus_list_view, len(todos)-1)
+
+    def update_list_display(self):
+        self.clear()
+
+        if len(self.todos) == 0:
+            self.append(TodoListItem("No todos in list! Press 'a' to add one."))
+            return
+
+        for todo in self.todos:
+            list_item = TodoListItem(todo)
+            self.append(list_item)
+    
     def move_todo(self, move_by: int):
-        index = self.todo_list_view.index
+        index = self.index
         item = self.todos.pop(index)
         self.todos.insert(index+move_by, item)
-        self.save_todos()
-        self.display_todos()
-        self.call_after_refresh(self.restore_focus, index + move_by)
-
-    def restore_focus(self, index: int):
-        self.todo_list_view.index = index
-        self.todo_list_view.focus()
+        export_todo_list(todos)
+        self.update_list_display()
+        self.call_after_refresh(self.app.focus_list_view, index + move_by)
 
     def action_move_todo_up(self):
         self.move_todo(-1)
 
     def action_move_todo_down(self):
         self.move_todo(1)
-        
-    def reload_todos(self):
-        todos = import_todo_list()
+
+
+
 
 class SelectCategoryScreen(ModalScreen[None]):
     BINDINGS = [
@@ -153,13 +180,11 @@ class SelectCategoryScreen(ModalScreen[None]):
         index = self.list_view.index
         label = self.list_view.children[index].children[0]
         set_category(label)
-        
-        
 
-class TodoListView(ListView):
+class DoneListView(ListView):
     def __init__(self):
         super().__init__(initial_index=1)
-        self.border_title = APP_TITLE
+        self.border_title = "Done"
         self.styles.border = ("heavy", "white")
 
 class TodoListItem(ListItem):
