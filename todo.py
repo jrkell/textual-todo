b8e2724b8f75db32c9630d1a79810cdd5313df50
Jared Kelly <jared.kelly@deftpower.com>
2026-03-18 15:23:32 +0100

    add categories

--- todo.py
+++ todo.py
@@ -6,6 +6,7 @@ from textual.app import App
 from textual.containers import Container, Vertical, Horizontal
 from textual.screen import ModalScreen
 from textual.widgets import Footer, Header, Input, Label, ListView, ListItem, Static
+from pathlib import Path
 sys.stdout.reconfigure(encoding='utf-8')
 
 ### CONFIG ###
@@ -15,8 +16,6 @@ SAVE_FILE_DIR = f"{current_dir}/todo-lists"
 CURRENT_LIST_FILE = f"{current_dir}/current-list.txt"
 APP_TITLE = "Jared's Todo List"
 ##############
-
-global CURRENT_LIST
 
 class TodoApp(App):
     CSS = """
@@ -36,14 +35,16 @@ class TodoApp(App):
     TITLE = APP_TITLE
     
     BINDINGS = [
-        ("q", "quit", "quit")
+        ("q", "quit", "quit"),
+        ("c", "select_category", "select category"),
     ]
 
     theme = "dracula"
 
     def __init__(self):
         super().__init__()
-        self.list_manager = ListManager(self)
+        self.category_manager = CategoryManager()
+        self.list_manager = ListManager(self, self.category_manager)
         self.todo_list_view = TodoListView(self.list_manager)
         self.done_list_view = DoneListView(self.list_manager)
         self.input = Input()
@@ -60,6 +61,11 @@ class TodoApp(App):
 
     def on_mount(self):
         self.update_lists()
+
+    def reload_lists_from_files(self):
+        self.list_manager.load_lists()
+        self.todo_list_view.update_list_display()
+        self.done_list_view.update_list_display()
 
     def update_lists(self):
         self.todo_list_view.update_list_display()
@@ -78,26 +84,53 @@ class TodoApp(App):
         self.input.focus()
         
     def action_select_category(self):
-        self.push_screen(SelectCategoryScreen())
+        self.push_screen(SelectCategoryScreen(self.category_manager))
+
+class CategoryManager():
+    def __init__(self):
+        self.current_list_name: str = open(CURRENT_LIST_FILE, "r", encoding='utf-8').read()
+        self.list_names = self.get_list_names()
+
+    def get_list_names(self):
+        list_names = []
+        for file in listdir(SAVE_FILE_DIR):
+            if file.endswith(".txt") and not file.endswith(".done.txt"):
+                list_names.append(file.split(".txt")[0])
+        return list_names
+
+    def switch_list(self, index: int):
+        self.current_list_name = self.list_names[index]
+        with open(CURRENT_LIST_FILE, "w", encoding='utf-8') as file:
+            file.write(self.current_list_name)
+
+    def add_new_list(self, name: str):
+        Path(f"{SAVE_FILE_DIR}/{name}.txt").touch()
+        Path(f"{SAVE_FILE_DIR}/{name}.done.txt").touch()
 
 class ListManager():
-    def __init__(self, app: TodoApp):
+    def __init__(self, app: TodoApp, category_manager: CategoryManager):
         self.app = app
-        self.current_list_name: str = open(CURRENT_LIST_FILE, "r", encoding='utf-8').read()
+        self.category_manager = category_manager
+        # self.current_list_name: str = category_manager.current_list_name
+        self.load_lists()
 
-        with open(f"{SAVE_FILE_DIR}/{self.current_list_name}.txt", "r", encoding='utf-8') as file:
+    def load_lists(self):
+        current_list_name = self.category_manager.current_list_name
+        with open(f"{SAVE_FILE_DIR}/{current_list_name}.txt", "r", encoding='utf-8') as file:
             self.todo_list = [i.strip() for i in file.readlines()]
 
-        with open(f"{SAVE_FILE_DIR}/{self.current_list_name}.done.txt", "r", encoding='utf-8') as file:
+        with open(f"{SAVE_FILE_DIR}/{current_list_name}.done.txt", "r", encoding='utf-8') as file:
             self.done_list: list[str] = [i.strip()for i in file.readlines()]
 
     def save_todo_list(self):
-        with open(f"{SAVE_FILE_DIR}/{self.current_list_name}.txt", "w", encoding='utf-8') as file:
+        current_list_name = self.category_manager.current_list_name
+        with open(f"{SAVE_FILE_DIR}/{current_list_name}.txt", "w", encoding='utf-8') as file:
             for todo in self.todo_list:
                 file.write(todo + "\n")
 
     def save_done_list(self):
-        with open(f"{SAVE_FILE_DIR}/{self.current_list_name}.done.txt", "w", encoding='utf-8') as file:
+        current_list_name = self.category_manager.current_list_name
+        with open(f"{SAVE_FILE_DIR}/{current_list_name}.done.txt", "w", encoding='utf-8') as file:
             for done in self.done_list:
                 file.write(done + "\n")
 
@@ -127,7 +160,6 @@ class TodoListView(ListView):
         ("a", "add_todo", "add"),
         ("d", "move_to_done", "move to done"),
         ("e", "edit_todo", "edit"),
-        # ("c", "select_category", "select category"),
         ("shift+up", "move_todo_up", "move up"),
         ("shift+down", "move_todo_down", "move down")
     }
@@ -202,9 +234,10 @@ class DoneListView(ListView):
             self.append(TodoListItem("No completed tasks yet!"))
             return
         
-        # ["date::blah", "date::blah2", "date2::bleh"]
+        sorted_dones = sorted(dones, reverse=True) # sort by most recent
+        
         date_groups = defaultdict(list)
-        for done in dones:
+        for done in sorted_dones:
             split_done = done.split("::")
             date = datetime.fromisoformat(split_done[0]).strftime("%Y-%m-%d")
             item = split_done[1]
@@ -220,16 +253,15 @@ class DoneListView(ListView):
 class SelectCategoryScreen(ModalScreen[None]):
     BINDINGS = [
         ("q", "app.pop_screen", "close window"),
-        ("enter", "select_category", "select category")    
+        ("space", "switch_category", "select category")    
     ]
     
     DEFAULT_CSS = """
-    SelectListScreen {
+    SelectCategoryScreen {
         align: center middle;
     }
-    
+
     #select-list-screen {
-        align: center middle;
         width: auto;
         max-width: 70%;
         height: auto;
@@ -238,6 +270,12 @@ class SelectCategoryScreen(ModalScreen[None]):
     """
     
     list_view = ListView(initial_index=1)
+    list_view.styles.border = ("heavy", "white")
+    list_view.border_title = "Select Category"
+    
+    def __init__(self, category_manager: CategoryManager):
+        super().__init__()
+        self.category_manager = category_manager
 
     def compose(self):
         with Container(id="select-list-screen"):
@@ -245,29 +283,20 @@ class SelectCategoryScreen(ModalScreen[None]):
             yield Footer()
             
     def _on_mount(self):
-        list_items = [ListItem(Label(i)) for i in get_category_names()]
+        list_items = [ListItem(Label(i)) for i in self.category_manager.get_list_names()]
         for item in list_items:
             self.list_view.append(item)
             
-    def action_select_category(self):
-        index = self.list_view.index
-        label = self.list_view.children[index].children[0]
-        set_category(label)
+    def action_switch_category(self):
+        self.category_manager.switch_list(self.list_view.index)
+        self.app.reload_lists_from_files()
+        self.app.pop_screen()
 
 class TodoListItem(ListItem):
     def __init__(self, label: str):
         super().__init__(Label(label))
 
-def set_category(category: str) -> None:
-    CURRENT_LIST = category # todo fix
-    with open(f"{CURRENT_LIST_FILE}.txt", "w", encoding='utf-8') as file:
-        file.write(category)
-
-def get_category_names() -> list[str]:
-    files = listdir(SAVE_FILE_DIR)
-    return [f.split(".txt")[0] for f in files]
 
 if __name__ == "__main__":
-    CURRENT_LIST = open(CURRENT_LIST_FILE, "r", encoding='utf-8').read() 
     app = TodoApp()
     app.run()
